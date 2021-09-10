4d1b281a183eafc80610ac753158be23f1beeefb
Ankit <40290144+akharb1192@users.noreply.github.com>
2021-09-10 09:23:13 +0530

    Update user_controller.go

--- restapi/pkg/controllers/user_controller.go
+++ restapi/pkg/controllers/user_controller.go
@@ -41,10 +41,16 @@ func UpdateUser(params user.PutUserParams) *models.Users {
 	return users
 }
 
-func LogInUser(params authorization.PostLoginParams) {
-	// user := models.Users {
-	// 	Email: params.Email,
-	// 	Password: strfmt.Password(params.Password),
-	// }
-
+func LogInUser(params authorization.PostLoginParams) int {
+	
+	count := int64(0)
+	user := models.Users{}
+	email1 := params.Body.Email
+	pass1 := params.Body.Password
+	database.DB.Model(user).Where("email = ? AND `password` = ?", email1, pass1).Count(&count)
+	if count > 0 {
+		return 1
+	} else {
+		return 0
+	}
 }
